2c3321abbe8341cb75aabee1bc423f190646c667
pyaephyomaung <samuelfruit@gmail.com>
2023-11-24 10:06:07 +0900

    Floor Plan rotation Algorithm

--- FloorPlanRotation/FloorPlanRotation/src/main/java/Main.java
+++ FloorPlanRotation/FloorPlanRotation/src/main/java/Main.java
@@ -1,111 +1,119 @@
 public class Main{
 
-    static String[][] originalFloor = new String[5][5];
-    public static void main(String[] args) {
+    private static String[][] originalFloor ;
+    private static int floorSize = 5;
+    private static int rotateStartRow = 2;
+    private static int getRotateStartCol = 2;
 
-        changeFloorPlan();
+    private static int rotateSize = 3;
+
+    private static int rotateDegree = 90;
+    public static void main(String[] args) {
+        rotateFloor(rotateDegree);
     }
 
-    static void changeFloorPlan(){
-
-        int floorSize = 5;
-        int startRow = 2;
-        int startColumn = 2;
-        int rotateSize = 3;
-        int degree = 90;
-
-
+    private static void rotateFloor(int degree){
 
         originalFloor = new String[][]{
-                {"1","1","1","1","1"},
+                {"0","0","0","0","0"},
                 {"0","1","2","3","0"},
-                {"1","4","5","6","1"},
-                {"0","7","8","9","0"}
+                {"0","4","5","6","0"},
+                {"0","7","8","9","0"},
+                {"0","0","0","0","0"},
         };
 
-        System.out.println("**Original Plan**");
-        for(String[] floorRows:originalFloor){
-            for(String vals:floorRows){
-                System.out.print("  "+vals+" ");
+        System.out.println("Original Floor Plan");
+        for(int i = 0;i<floorSize;i++){
+            for(int j =0;j<floorSize;j++){
+                System.out.print(originalFloor[i][j]+" ");
             }
             System.out.println();
-        }
-
-        String[][] rotatedPortion = new String[rotateSize][rotateSize];
-
-        for (int i = 0;i<rotateSize;i++){
-            for (int j = 0;j<rotateSize;j++){
-
-                rotatedPortion[i][j] = originalFloor[i+(startRow - 1)][j+(startColumn-1)] ;
-            }
         }
         System.out.println();
-        System.out.println("**Changed Portion**");
-        for(String[] floorRows:rotatedPortion){
-            for(String vals:floorRows){
-                System.out.print("  "+vals+" ");
+
+        int rotateTimes = degree/90;
+
+        for(int i=0;i<rotateTimes;i++){
+            singleRotation();
+        }
+
+    }
+
+    private static void singleRotation(){
+
+        System.out.println("Rotation started!!");
+
+        String[][] changedPortion = new String[rotateSize][rotateSize];
+
+        for(int i = 0;i<rotateSize;i++){
+            for(int j =0;j<rotateSize;j++){
+                changedPortion[i][j] = originalFloor[i + (rotateStartRow - 1)][j + (getRotateStartCol -1)];
+            }
+        }
+
+        for(int i = 0;i<rotateSize;i++){
+            for(int j =0;j<rotateSize;j++){
+                System.out.print(changedPortion[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+        for(int i = 0;i<rotateSize;i++){
+            for(int j =0;j<rotateSize;j++){
+                if (i==0){
+                    rotateTopRow(changedPortion[i][j],j);
+                }else if(i== (rotateSize-1)){
+                    rotateBottomRow(changedPortion[i][j],j);
+                }else if(j==0){
+                    rotateLeftColumn(changedPortion[i][j],i);
+                }else if(j== (rotateSize - 1)){
+                    rotateRightColumn(changedPortion[i][j],i);
+                }
+            }
+        }
+
+        System.out.println("Rotation End!!");
+
+        for(int i = 0;i<floorSize;i++){
+            for(int j =0;j<floorSize;j++){
+                System.out.print(originalFloor[i][j]+" ");
             }
             System.out.println();
         }
 
         System.out.println();
-        System.out.println("Changed Portion Confirmed");
-
-        for (int i = 0;i<rotateSize;i++){
-            for (int j = 0;j<rotateSize;j++){
-
-                if(i==0){
-                    processTopRow(rotatedPortion[i][j],i,j,startRow,startColumn,rotateSize);
-                }
-                else if(i==(rotateSize - 1)){
-                    processBottomRow(rotatedPortion[i][j],i,j,startRow,startColumn,rotateSize);
-                }
-                else if(j== 0){
-                    processLeft(rotatedPortion[i][j],i,j,startRow,startColumn,rotateSize);
-                }
-                else if(j== (rotateSize -1)){
-                    processRight(rotatedPortion[i][j],i,j,startRow,startColumn,rotateSize);
-                }
-            }
-        }
-
-        System.out.println("**Final Plan**");
-        for(String[] floorRows:originalFloor){
-            for(String vals:floorRows){
-                System.out.print("  "+vals+" ");
-            }
-            System.out.println();
-        }
-
 
     }
 
-    static private void processTopRow(String floorData,int oldRow,int oldCOl,int rotateRow,int rotateCol,int size){
-        int newRow = (oldCOl + (rotateRow -1));
-        int newCol = (size - 1) + (rotateCol - 1);
+    private static void rotateTopRow(String floorContent,int changeCol){
+        int newRow = (rotateStartRow -1) + changeCol;
+        int newColumn = (getRotateStartCol - 1) + (rotateSize - 1);
 
-        originalFloor[newRow][newCol] = floorData;
+        originalFloor[newRow][newColumn] = floorContent;
+    }
+
+    private static void rotateBottomRow(String floorContent,int changeCol){
+        int newRow = (rotateStartRow -1) + changeCol;
+        int newColumn = (getRotateStartCol - 1) ;
+
+        originalFloor[newRow][newColumn] = floorContent;
 
     }
 
-    static private void processBottomRow(String floorData,int oldRow,int oldCOl,int rotateRow,int rotateCol,int size){
-        int newRow = (oldCOl + (rotateRow -1));
-        int newCol = (rotateCol - 1);
+    private static void rotateLeftColumn(String floorContent,int changeRow){
+        int newRow = (rotateStartRow -1) ;
+        int newColumn = (getRotateStartCol - 1) + changeRow ;
 
-        originalFloor[newRow][newCol] = floorData;
+        originalFloor[newRow][newColumn] = floorContent;
+
     }
 
-    static private void processLeft(String floorData,int oldRow,int oldCOl,int rotateRow,int rotateCol,int size){
-        int newRow = (rotateRow -1);
-        int newCol = (oldRow)+(rotateCol - 1);
+    private static void rotateRightColumn(String floorContent,int changeRow){
+        int newRow = (rotateStartRow -1) + (rotateSize - 1) ;
+        int newColumn = (getRotateStartCol - 1) + changeRow ;
 
-        originalFloor[newRow][newCol] = floorData;
-    }
+        originalFloor[newRow][newColumn] = floorContent;
 
-    static private void processRight(String floorData,int oldRow,int oldCOl,int rotateRow,int rotateCol,int size){
-        int newRow = (size -1) + (rotateRow -1);
-        int newCol = (oldRow)+(rotateCol - 1);
-
-        originalFloor[newRow][newCol] = floorData;
     }
 }
